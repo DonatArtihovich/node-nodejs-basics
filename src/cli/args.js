5c8d602dfab5355e349e7f5d551369bb0ae7ae90
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-06-17 07:18:13 +0300

    feat: complete cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    let str = '';
+
+    for (let i = 0; i < args.length; i += 2) {
+        str += (i === args.length - 2)
+            ? `${args[i].split('').slice(2).join('')} is ${args[i + 1]}`
+            : `${args[i].split('').slice(2).join('')} is ${args[i + 1]}, `
+    }
+
+    console.log(str);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    const keys = Object.keys(process.env);
+    const values = Object.values(process.env);
+    const strArr = [];
+
+    for (let i = 0; i < keys.length; i++) {
+        strArr.push(`RSS_${keys[i]}=${values[i]}; `);
+    }
+
+    console.log(strArr.join(''));
 };
 
 parseEnv();
