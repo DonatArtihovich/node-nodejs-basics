6d52f764354ad1a7a734d221f75549758103775e
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-06-17 07:39:15 +0300

    feat: complete write and read streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+const { stdout } = process;
+import fs from 'node:fs'
+import path from 'node:path'
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = fs.createReadStream(path.resolve('files', 'fileToRead.txt'));
+    readableStream.on('data', (data) => {
+        stdout.write(data);
+    })
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import path from 'node:path'
+import fs from 'node:fs'
+const { stdin } = process;
+
+
 const write = async () => {
-    // Write your code here 
+    const writeableStream = fs.createWriteStream(path.resolve('files', 'fileToWrite.txt'));
+    stdin.on('data', data => {
+        writeableStream.write(data);
+    })
 };
 
 await write();
