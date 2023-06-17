4ee36a83722ac02af4f94691e21e601ed65db6de
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-06-17 22:23:38 +0300

    feat: complete zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs'
+import zlib from 'node:zlib'
+import path from 'node:path'
+import { pipeline } from 'node:stream'
+
 const compress = async () => {
-    // Write your code here 
+    const readableStream = fs.createReadStream(path.resolve('files', 'fileToCompress.txt'));
+    const writableStream = fs.createWriteStream(path.resolve('files', 'archive.gz'));
+    const gzip = zlib.createGzip();
+
+    pipeline(readableStream, gzip, writableStream, err => {
+        if (err) throw err;
+    });
+    // readableStream.pipe(gzip).pipe(writableStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs'
+import { pipeline } from 'node:stream'
+import path from 'node:path'
+import zlib from 'node:zlib'
+
 const decompress = async () => {
-    // Write your code here 
+    const readableStream = fs.createReadStream(path.resolve('files', 'archive.gz'));
+    const writableStream = fs.createWriteStream(path.resolve('files', 'fileToCompress.txt'));
+    const gunzip = zlib.createGunzip();
+
+    pipeline(readableStream, gunzip, writableStream, err => {
+        if (err) throw err
+    });
 };
 
 await decompress();
