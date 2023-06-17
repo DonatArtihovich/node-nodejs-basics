ed31351bf95522e0cc324ad1cc4aa5845d0d801e
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-06-17 21:35:10 +0300

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs'
+import crypto from 'node:crypto'
+import path from 'node:path'
+const { stdout } = process;
+
 const calculateHash = async () => {
-    // Write your code here 
+    const readableStream = fs.createReadStream(path.resolve('files', 'fileToCalculateHashFor.txt'));
+    const hash = crypto.createHash('sha256');
+    readableStream.on('data', (data) => {
+        hash.update(data);
+    })
+    readableStream.on('end', () => { stdout.write(hash.digest('hex')) });
 };
 
 await calculateHash();
