2a71ca253935f79841c1bd6f6c4eebedfbc8a592
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-06-19 16:10:34 +0300

    feat: complete worker treads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,42 @@
+import os from 'node:os'
+import { Worker } from 'node:worker_threads'
+import EventEmitter from 'node:events'
+const emitter = new EventEmitter()
+emitter.on('messages', () => { console.log(resultsArr) })
+
+const workersArr = [];
+const resultsArr = [];
+let counter = 0;
+
 const performCalculations = async () => {
-    // Write your code here
+    const numOfWorkers = os.cpus().length;
+
+    for (let i = 0; i < numOfWorkers; i++) {
+        const worker = new Worker('./worker.js');
+        worker.on('message', data => {
+            resultsArr.push({
+                status: 'resolved',
+                data: data
+            })
+
+            counter++
+            if (counter === numOfWorkers) emitter.emit('messages')
+        });
+
+        worker.on('error', () => {
+            resultsArr.push({
+                status: 'error',
+                data: null
+            })
+        });
+
+        workersArr.push(worker);
+    }
+
+    workersArr.reduce((num, worker) => {
+        worker.postMessage(num);
+        return num + 1
+    }, 10);
 };
 
 await performCalculations();
